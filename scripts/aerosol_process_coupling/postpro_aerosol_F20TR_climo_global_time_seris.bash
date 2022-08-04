74cd584494188cc032fecdc7f82dc6e2bc07780b
Hui Wan <hui.wan@pnnl.gov>
2022-08-04 09:49:30 -0700

    zppy postpro: use the current E3SM unified env to avoid problem when processing global time series

---
+++ scripts/aerosol_process_coupling/postpro_aerosol_F20TR_climo_global_time_seris.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -fe
+
+#--------------------------------------------------------------------------
+# Notes by Hui Wan @PNNL, July-August 2022:
+#
+# This simple script is based on Chris Terai's advice from 7/20/2022.
+#
+# We load the E3SM Unified environment on Compy, 
+# then use zppy to process years 1985-2014 of an AMIP simulation and 
+# compare the mean climate agaist the observations and the CMIP6 model ensemble.
+#  
+# The .cfg file for box plot and Taylor diagram was adapted from a template provided by Qi Tang and Jill Zhang from LLNL.
+# The .cfg file for more diags was adapted from Chris Terai's example.
+#-----------------------------------------------------------------------------
+ 
+
+source /share/apps/E3SM/conda_envs/load_latest_e3sm_unified_compy.sh
+zppy -c zppy.post.20220519.v2.LR.bi-grid.amip.cflx_cpl_opt_2.compy.model_vs_obs_1985-2014_global_time_series.cfg
